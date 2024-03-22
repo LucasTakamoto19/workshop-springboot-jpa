fa27159622c3bf3ebf22460a0bfb842b1bbb143e
Lucas Takamoto <lucas.takamoto@hotmail.com>
2024-03-22 11:34:06 -0300

    Criação do service e do componente

---
+++ src/main/java/com/projetoudemy/curso/services/UserService.java
@@ -0,0 +1,25 @@
+package com.projetoudemy.curso.services;
+
+import com.projetoudemy.curso.entities.User;
+import com.projetoudemy.curso.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User>findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
